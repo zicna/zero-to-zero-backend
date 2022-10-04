08aa12d7c0a0332246f67225ed964a5ade371d6d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-10-04 09:50:01 -0500

    add dummy data to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_183856) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_143446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,11 +38,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_183856) do
   create_table "questions", force: :cascade do |t|
     t.bigint "level_id", null: false
     t.bigint "language_id", null: false
-    t.string "name"
     t.string "description"
     t.string "git_hub_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["language_id"], name: "index_questions_on_language_id"
     t.index ["level_id"], name: "index_questions_on_level_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "📃 Seeding data..."
+
+languages = [
+    {name: "JavaScript"},{name: "Ruby"},{name: "Java"}
+]
+
+languages.map do |lang|
+    Language.find_or_create_by(name: lang[:name]) do |l|
+        l.name = lang[:name]
+        puts("language #{l.name} was created")
+    end
+end
+
+levels = [
+    {name: "Intership"},
+    {name: "Junior"},
+    {name: "Medior"},
+    {name: "Senior"},
+    {name: "Principal"}
+]
+
+levels.map do |level|
+    Level.find_or_create_by(name: level[:name]) do |l|
+        l.name = level[:name]
+        puts("level #{l.name} was created")
+    end
+end
+
+questions = [
+    {language_id: 1, level_id: 1, title: "Hoisting in JS", description: "Explain hoisting in JavaScript, with example of your choice", git_hub_link: ""}
+]
+
+questions.map do |question|
+    Question.find_or_create_by(title: question[:title]) do |q|
+        q.language_id = question[:language_id]
+        q.level_id = question[:level_id]
+        q.title = question[:title]
+        q.description = question[:description]
+        q.git_hub_link = question[:git_hub_link]
+    end
+end
+
+
+puts "✅ ✅ Done seeding ✅ ✅"
